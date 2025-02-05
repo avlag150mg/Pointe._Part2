6579538a6b7b4cb453b1a050681079de9a7c5ac8
student <starbravl405@gmail.com>
2025-02-05 20:38:18 +0200

    +

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,48 @@
-#include <iostream> 
-#include <Windows.h> 
+#include <iostream>
+#include <set>
+#include <vector>
+#include <Windows.h>
 using namespace std; 
  
 int main() { 
     SetConsoleCP(1251); 
     SetConsoleOutputCP(1251); 
+    int M, N;
     
+    cout << "Введіть розмір масиву A: ";
+    cin >> M;
+    cout << "Введіть розмір масиву B: ";
+    cin >> N;
+
+    int A[M], B[N];
+    
+    cout << "Введіть елементи масиву A: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+    
+    cout << "Введіть елементи масиву B: ";
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+    
+    set<int> setA(A, A + M);
+    set<int> intersection;
+
+    for (int i = 0; i < N; i++) {
+        if (setA.count(B[i])) {
+            intersection.insert(B[i]);
+        }
+    }
+
+    vector<int> C(intersection.begin(), intersection.end());
+
+    cout << "Спільні елементи без повторень: ";
+    for (int elem : C) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
     return 0; 
 }
 
