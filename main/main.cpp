2a80b91cd44b7b8da19081367166ee6ae059cee1
student <starbravl405@gmail.com>
2025-02-05 20:41:56 +0200

    +

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,46 @@
-#include <iostream> 
+#include <iostream>
+#include <set>
+#include <vector>
 #include <Windows.h> 
 using namespace std; 
  
 int main() { 
     SetConsoleCP(1251); 
     SetConsoleOutputCP(1251); 
+    int M, N;
     
+    cout << "Введіть розмір масиву A: ";
+    cin >> M;
+    cout << "Введіть розмір масиву B: ";
+    cin >> N;
+
+    int A[M], B[N];
+    
+    cout << "Введіть елементи масиву A: ";
+    for (int i = 0; i < M; i++) {
+        cin >> A[i];
+    }
+    
+    cout << "Введіть елементи масиву B: ";
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+    
+    set<int> setA(A, A + M);
+    set<int> setB(B, B + N);
+    vector<int> C;
+    
+    for (int elem : setA) {
+        if (setB.find(elem) == setB.end()) {
+            C.push_back(elem);
+        }
+    }
+    
+    cout << "Елементи масиву A, яких немає в B (без повторень): ";
+    for (int elem : C) {
+        cout << elem << " ";
+    }
+    cout << endl;
     return 0; 
 }
 
